c09b0abe8155384df1b339d1217c610fd2559f90
Noahrng <araneganoah@gmail.com>
2025-05-28 16:14:33 +0200

    résoolution du bug de scroll

--- pages/page_connexion.php
+++ pages/page_connexion.php
@@ -1,30 +1,39 @@
 <?php
     session_start();
     
+    //debug
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+    
+    //Config.php pour se connecter à la base de données
     require '../base_de_donnees/config.php';
 
+    //On initialise un message d'erreur et un message de succès pour savoir si la requête est réussie ou non
     $m_erreur = "";
     $m_succes="";
 
+    //Si le serveur reçoit une requête POST
     if($_SERVER["REQUEST_METHOD"]  == "POST"){
+
+
         $login =  htmlspecialchars(trim($_POST['login'])); //htmlspecialchar permet d'éviter les injections de script malveillants
         //trim supprime les espaces inutiles
         $mot_de_passe = htmlspecialchars($_POST['mdp']);
 
+        //On prépare la requête qui permet de récupérer l'id et le mot de passe haché du login
         $rqt = mysqli_prepare($connexion, "SELECT id, password FROM users WHERE username = ?");
-        mysqli_stmt_bind_param($rqt, "s", $login);
-        mysqli_stmt_execute($rqt);
-        $result = $rqt->get_result();
+        mysqli_stmt_bind_param($rqt, "s", $login); //On affecte le paramètre login de type string (s) 
+        mysqli_stmt_execute($rqt); //exécution
+        $result = mysqli_stmt_get_result($rqt); //On récupère le résultat
 
-        if($row = $result->fetch_assoc()){
-            if(password_verify($mot_de_passe, $row['password'])){
-                $_SESSION['login'] = $login;
-                $_SESSION['id'] = $row['id'];  
-                $m_succes = "Connexion en cours...";
-                header("refresh:2;url=page_images.php");
+        //fetch_assoc transforme $result en un tableau 2D (1D pour cette fois car il n'y a  qu'un seul utilisateur)
+        if($row = mysqli_fetch_assoc($result)){
+            if(password_verify($mot_de_passe, $row['password'])){ //On vérifie si le haché correspond au mot de passe entrée
+                $_SESSION['login'] = $login; //Le login de la session = le login en entrée
+                $_SESSION['id'] = $row['id'];  //l'id de la session = l'id en entrée (récupéré grâce à la requête sql)
+                $m_succes = "Connexion en cours..."; 
+                header("refresh:2;url=page_images.php"); //Redirection de 2 secondes vers l'url page_images.php
             } else {
                 $m_erreur = "Mot de passe incorrect !";
             }
@@ -46,8 +55,7 @@
     </head>
     <body>
         <?php
-            require '../base_de_donnees/config.php';
-            include("../include_php/en-tete.php");
+            include("../include_php/en-tete.php"); //On inclut l'en-tête
         ?>
         <p class="bienvenue">Bienvenue sur L'Œil d'Or. Connectez-vous pour accéder à vos images.</p>
         <div class="contenu-sans-barre-lateral">
